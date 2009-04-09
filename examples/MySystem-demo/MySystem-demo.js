0b0560102983137c2955a99b2c84452678abe545
Chad Dorsey <cdorsey@concord.org>
2009-04-08 22:30:43 -0400

    Adding initial energy form label as extension of jsBox container

--- examples/MySystem-demo/MySystem-demo.js
+++ examples/MySystem-demo/MySystem-demo.js
@@ -12,7 +12,7 @@ var jsBox = {
 		],
 		modules: [
 		   {
-		      "name": "New Element",
+		      "name": "Energy Form Label",
 		      "container": {"xtype": "jsBox.Container"}
 		   },
 			{
@@ -210,16 +210,9 @@ jsBox.Container = function(options, layer) {
    jsBox.Container.superclass.constructor.call(this, options, layer);
 
 
-   this.buildTextArea(options.codeText || "function(e){} Label me here!");
+   this.buildTextArea(options.codeText || "Label me!");
    /**this.buildTextArea(options.codeText || "Label Me!"); **/
-   
-   this.createTerminals();
-   
-   // Reposition the terminals when the jsBox is being resized
-   this.ddResize.eventResize.subscribe(function(e, args) {
-      this.positionTerminals();
-      YAHOO.util.Dom.setStyle(this.textarea, "height", (args[0][1]-70)+"px");
-   }, this, true);
+
 };
 
 YAHOO.extend(jsBox.Container, WireIt.Container, {
@@ -231,78 +224,11 @@ YAHOO.extend(jsBox.Container, WireIt.Container, {
     */
    buildTextArea: function(codeText) {
 
-      this.textarea = WireIt.cn('textarea', null, {width: "90%", height: "70px", border: "0", padding: "5px"}, codeText);
+      this.textarea = WireIt.cn('textarea', null, {width: "70%", height: "20px", border: "0", padding: "5px"}, codeText);
       this.setBody(this.textarea);
 
       YAHOO.util.Event.addListener(this.textarea, 'change', this.createTerminals, this, true);
       
-   },
-   
-   /**
-    * Create (and re-create) the terminals with this.nParams input terminals
-    * @method createTerminals
-    */
-   createTerminals: function() {
-      
-      // Output Terminal
-      if(!this.outputTerminal) {
-   	   this.outputTerminal = this.addTerminal({xtype: "WireIt.util.TerminalOutput", "name": "out"});      
-         this.outputTerminal.jsBox = this;
-      }
-      
-      // Input terminals :
-      var match = (this.textarea.value).match((/^[ ]*function[ ]*\((.*)\)[ ]*\{/));
-   	var sParamList = match ? match[1] : "";
-      var params = sParamList.split(',');
-      var nParams = (sParamList=="") ? 0 : params.length;
-      
-      var curTerminalN = this.nParams || 0;
-      if(curTerminalN < nParams) {
-         // add terminals
-         for(var i = curTerminalN ; i < nParams ; i++) {
-            var term = this.addTerminal({xtype: "WireIt.util.TerminalInput", "name": "param"+i});
-            //term.jsBox = this;
-            WireIt.sn(term.el, null, {position: "absolute", top: "-15px"});
-         }
-      }
-      else if (curTerminalN > nParams) {
-         // remove terminals
-         for(var i = this.terminals.length-(curTerminalN-nParams) ; i < this.terminals.length ; i++) {
-         	this.terminals[i].remove();
-         	this.terminals[i] = null;
-         }
-         this.terminals = WireIt.compact(this.terminals);
-      }
-      this.nParams = nParams;
-   
-      this.positionTerminals();
-
-      // Declare the new terminals to the drag'n drop handler (so the wires are moved around with the container)
-      this.dd.setTerminals(this.terminals);
-   },
-   
-   /**
-    * Reposition the terminals
-    * @method positionTerminals
-    */
-   positionTerminals: function() {
-      var width = WireIt.getIntStyle(this.el, "width");
-
-      var inputsIntervall = Math.floor(width/(this.nParams+1));
-
-      for(var i = 1 ; i < this.terminals.length ; i++) {
-         var term = this.terminals[i];
-         YAHOO.util.Dom.setStyle(term.el, "left", (inputsIntervall*(i))-15+"px" );
-         for(var j = 0 ; j < term.wires.length ; j++) {
-            term.wires[j].redraw();
-         }
-      }
-      
-      // Output terminal
-      WireIt.sn(this.outputTerminal.el, null, {position: "absolute", bottom: "-15px", left: (Math.floor(width/2)-15)+"px"});
-      for(var j = 0 ; j < this.outputTerminal.wires.length ; j++) {
-         this.outputTerminal.wires[j].redraw();
-      }
    },
    
    /**
